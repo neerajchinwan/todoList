4b0f0eab8507a6eb1c258d2b9c54340abd1b0eee
neerajchinwan <neeraj14chinwan@gmail.com>
2024-04-23 00:56:54 +0530

    connection string change

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use('*', (req, res) => {
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/todoDatabase');
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
     console.log('database is connected successfully')
 }
 
